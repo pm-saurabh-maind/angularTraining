c219afadfb60fcb0722126bf1dfc6ec4fc01cb42
Saurabh Maind <saurabh.maind@pubmatic.com>
2015-04-15 13:15:48 +0530

    Angular ng-repeat example

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.repeatData = ["1", "2", "3", "4"];
+    $scope.statusList = ["asd", "ere"];
+  });
